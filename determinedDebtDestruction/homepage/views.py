885e0f3b18a7bc4e6b5d9a2402b8324dbbad8ec6
Dustin <Dnaylor04@gmail.com>
2021-09-13 19:44:16 -0400

    DjangoTest into Main
    After nuking, restored DjangoTest branch into main.

---
+++ determinedDebtDestruction/homepage/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'homepage/homepage.html')
+
+def about(request):
+    return render(request, 'homepage/about.html')
