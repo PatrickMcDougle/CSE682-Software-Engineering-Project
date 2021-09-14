8ca60c90405674f5a1f0ed9a9aaf9476f321b301
Dustin <Dnaylor04@gmail.com>
2021-09-14 07:51:59 -0400

    Final 1.0 MVP
    Added CRUD for Debt and Recurring Charges.  The Homepage now checks if the user is authenticated and will display their information as a summary.  If the user is not authenticated the values will all be 0 and will request the user to authenticate.

--- determinedDebtDestruction/debtManager/urls.py
+++ determinedDebtDestruction/debtManager/urls.py
@@ -2,9 +2,20 @@
     This is the URL.py file for the Debt Manager
 """
 from django.urls import path
+from .views import (
+    DebtListView,
+    DebtDetailView,
+    DebtCreateView,
+    DebtUpdateView,
+    DebtDeleteView,
+    )
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='debt-manager-home'),
+    path('', DebtListView.as_view(), name='debt-manager-home'),
+    path('<int:pk>/', DebtDetailView.as_view(), name='debt-detail'),
+    path('new/', DebtCreateView.as_view(), name='debt-create'), 
+    path('<int:pk>/update/', DebtUpdateView.as_view(), name='debt-update'), 
+    path('<int:pk>/delete/', DebtDeleteView.as_view(), name='debt-delete'),  
     path('about/', views.about, name='debt-manager-about'),
 ]
